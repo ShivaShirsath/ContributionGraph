add916ca8644f17ad7caff90e8d89ffd8d2a65cf
Shiva Shirsath <Shiva.S.Shirsath@gmail.com>
2020-12-21 21:26:37 +0530

    committed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const makeCommit = n => {
     if(n===0) return simpleGit().push();
     const x = random.int(0,54);
     const y = random.int(0,6);
-    const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format();
+    const DATE = moment().subtract(1,'y').add(1,'d').add(1,'w').add(1,'d').format();
     const data = {
         date: DATE
     }
